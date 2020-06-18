884cbd2530bf2e962b6ccbc9456cfc68c52d74c2
kierandesmond <34133202+kierandesmond@users.noreply.github.com>
2020-06-18 09:32:51 +0200

    Create example.js

---
+++ example.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function($){
+
+    if(jQuery(".et_pb_contact_username_hp_1").length) // change to the name / class of your input
+        {
+            jquery(".et_pb_contact_submit .et_pb_button .et_pb_custom_button_icon").prop( "disabled", true ); // this is the submit button
+        }
+}); // document ready
